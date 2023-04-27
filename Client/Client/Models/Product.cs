57a9a16a48e0146a97dfc4b1aedc412af06df0f7
Huwnga <hungbungbu2003@gmail.com>
2023-04-27 23:55:36 +0700

    Layout customer client

---
+++ Client/Client/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Client.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<int> MaterialID { get; set; }
+        public Nullable<short> UnitsInStock { get; set; }
+        public Nullable<short> UnitsOnOrder { get; set; }
+        public Nullable<short> ReoderLevel { get; set; }
+
+        public virtual Material Material { get; set; }
+    }
+}
