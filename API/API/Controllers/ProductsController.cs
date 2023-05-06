baa3196cae890616bc37a44157ee8f2929c4add4
Huwnga <hungbungbu2003@gmail.com>
2023-05-07 02:04:10 +0700

    product api and paged list  products

--- API/API/Controllers/ProductsController.cs
+++ API/API/Controllers/ProductsController.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Api.Models;
+using Microsoft.Ajax.Utilities;
+using PagedList;
 
 namespace Api.Controllers
 {
     public class ProductsController : ApiController
     {
+        private int pageSizeDeffault = 10;
         private MyImageEntities db = new MyImageEntities();
 
         // GET: api/Products
-        public IQueryable<Product> GetProducts()
+        public IPagedList<Product> GetProducts(int? pageNumber, int? pageSize)
         {
-            return db.Products;
+            int number = (pageNumber ?? 1);
+            int size = (pageSize ?? pageSizeDeffault);
+
+            return db.Products.ToPagedList(number, size);
         }
 
         // GET: api/Products/5
@@ -38,16 +47,63 @@ namespace Api.Controllers
 
         // GET: api/Products/Category
         [Route("api/Products/Category")]
-        public IQueryable<Product> GetProductsByCategoryID(int categoryID)
+        public IPagedList<ProductSize> GetProductsByCategoryID(int categoryID, int? pageNumber, int? pageSize)
         {
-            return db.Products.Where(e => e.CategoryID == categoryID);
+            var productSizes = db.ProductSizes
+                .Include(e => e.Product)
+                .OrderBy(e => e.ProductID)
+                .Where(e => e.Product.CategoryID == categoryID);
+
+            int number = (pageNumber ?? 1);
+            int size = (pageSize ?? pageSizeDeffault);
+            return productSizes.ToPagedList(number, size);
         }
 
         // GET: api/Products/BestSealer
         [Route("api/Products/BestSealer")]
-        public IQueryable<Product> GetProductsBestSealer()
+        public IPagedList<ProductSize> GetProductsBestSealer(int? pageNumber, int? pageSize)
         {
-            return db.Products.Where(e => e.CategoryID == categoryID);
+
+            List<ProductSize> lowerPriceProducts = new List<ProductSize>();
+            var productSizes = db.ProductSizes
+                .Include(e => e.Product)
+                .OrderByDescending(e => e.Product.ReoderLevel)
+                .ToList();
+
+            foreach (ProductSize productSize in productSizes)
+            {
+                if (!lowerPriceProducts.Contains(productSize))
+                {
+                    lowerPriceProducts.Add(productSize);
+                }
+            }
+
+            int number = (pageNumber ?? 1);
+            int size = (pageSize ?? pageSizeDeffault);
+            return lowerPriceProducts.ToPagedList(number, size);
+        }
+
+        // GET: api/Products/LowerPrice
+        [Route("api/Products/LowerPrice")]
+        public IPagedList<ProductSize> GetProductsLowerPrice(int? pageNumber, int? pageSize)
+        {
+            List<ProductSize> lowerPriceProducts = new List<ProductSize>();
+            var productSizes = db.ProductSizes
+                .Include(e => e.Product)
+                .OrderBy(e => e.UnitPrice)
+                .ToList();
+
+            foreach (ProductSize productSize in productSizes)
+            {
+                if (!lowerPriceProducts.Contains(productSize))
+                {
+                    lowerPriceProducts.Add(productSize);
+                }
+            }
+
+            int number = (pageNumber ?? 1);
+            int size = (pageSize ?? pageSizeDeffault);
+            return lowerPriceProducts.ToPagedList(number, size);
         }
 
         // PUT: api/Products/5
@@ -63,6 +119,8 @@ namespace Api.Controllers
             {
                 return BadRequest();
             }
+
+            product.UpdatedAt = DateTime.Now;
 
             db.Entry(product).State = EntityState.Modified;
 
@@ -93,6 +151,9 @@ namespace Api.Controllers
             {
                 return BadRequest(ModelState);
             }
+
+            product.CreatedAt = DateTime.Now;
+            product.UpdatedAt = DateTime.Now;
 
             db.Products.Add(product);
             await db.SaveChangesAsync();
---
+++ CusClient/CusClient/Api/ProductApi.cs
@@ -0,0 +1,51 @@
+﻿using CusClient.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace CusClient.Api
+{
+    public class ProductApi
+    {
+        public static IPagedList<ProductSize> GetProducts(int? pageNumber, int? pageSize)
+        {
+            ResClient resClient = new ResClient();
+            resClient.EndPoint = "api/Products?pageNumber=" + pageNumber + " && pageSize=" + pageSize;
+            string resStrProducts = resClient.RestRequestAll();
+            IPagedList<ProductSize> productSizes = JsonConvert.DeserializeObject<IPagedList<ProductSize>>(resStrProducts);
+
+            return productSizes;
+        }
+
+        public static IPagedList<ProductSize> GetProductsByCategoryID(int CategoryID, int? pageNumber, int? pageSize)
+        {
+            ResClient resClient = new ResClient();
+            resClient.EndPoint = "api/Products/Category?categoryID=" + CategoryID + " && pagenumber=" + pageNumber + " && pageSize=" + pageSize;
+            string resStrProducts = resClient.RestRequestAll();
+            IPagedList<ProductSize> productSizes = JsonConvert.DeserializeObject<IPagedList<ProductSize>>(resStrProducts);
+
+            return productSizes;
+        }
+        public static IPagedList<ProductSize> GetProductsBestSealer(int? pageNumber, int? pageSize)
+        {
+            ResClient resClient = new ResClient();
+            resClient.EndPoint = "api/Products/BestSealer?pageNumber=" + pageNumber + " && pageSize=" + pageSize;
+            string resStrProducts = resClient.RestRequestAll();
+            IPagedList<ProductSize> productSizes = JsonConvert.DeserializeObject<IPagedList<ProductSize>>(resStrProducts);
+
+            return productSizes;
+        }
+        public static IPagedList<ProductSize> GetProductsLowerPrice(int? pageNumber, int? pageSize)
+        {
+            ResClient resClient = new ResClient();
+            resClient.EndPoint = "api/Products/LowerPrice?pageNumber=" + pageNumber + " && pageSize=" + pageSize;
+            string resStrProducts = resClient.RestRequestAll();
+            IPagedList<ProductSize> productSizes = JsonConvert.DeserializeObject<IPagedList<ProductSize>>(resStrProducts);
+
+            return productSizes;
+        }
+    }
+}
